f494c1429e3f233e05ba3d98aa5ef04f52bff400
Alan Nunez <raul.nunez01@gmail.com>
2021-09-10 05:47:51 -0400

    Hello React

---
+++ src/CardList.js
@@ -0,0 +1,19 @@
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((user, i) => {
+        return (
+          <Card
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          ></Card>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
