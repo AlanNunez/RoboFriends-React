f9f2258bbd83eb15364de301dd6c8e6b90dee77a
Alan Nunez <raul.nunez01@gmail.com>
2021-09-11 07:51:36 -0400

    Fallback component

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 class App extends Component {
   constructor() {
@@ -29,14 +30,16 @@ class App extends Component {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    return robots.length ? (
+    return !robots.length ? (
       <h1>Loading</h1>
     ) : (
       <div className="tc">
         <h1>RobotFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filterRobots}></CardList>
+          <ErrorBoundary>
+            <CardList robots={filterRobots}></CardList>
+          </ErrorBoundary>
         </Scroll>
       </div>
     );
